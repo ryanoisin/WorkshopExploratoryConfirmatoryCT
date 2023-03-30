d724719d8a043b0266ba7f7a184d5cab8916ca06
ryanoisin <ryanoisin@users.noreply.github.com>
2023-03-30 15:33:23 +0200

    initial

---
+++ setup.R
@@ -0,0 +1,13 @@
+# Preperation for the workshop
+# Please install these packages well in advance of the workshop
+
+install.packages("ctsem", dependencies = TRUE)
+# note - installation of ctsem can be quite involved
+# - if throwing errors, you may need to follow the instructions/vignette found on: https://github.com/cdriveraus/ctsem
+
+install.packages("devtools")
+devtools::install_github("ryanoisin/ctnet")
+devtools::install_github("ryanoisin/expct")
+install.packages("qgraph")
+
+
